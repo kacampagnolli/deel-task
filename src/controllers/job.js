6f092845885e563cb5439f345eb875f67f0bfc6c
kacampagnolli <kacampagnolli@gmail.com>
2024-04-21 16:42:45 -0300

    add unpaid endpoint and fix job model and getProfile

--- src/controllers/job.js
+++ src/controllers/job.js
@@ -1,5 +1,25 @@
+const { profileToQuery } = require("../utils/query");
+
 const listUnpaid = async (req, res) => {
-  res.json({});
+  const profile = req.profile;
+  const { Job, Contract } = req.app.get("models");
+  console.log(req.app.get("models"));
+  const jobs = await Job.findAll({
+    include: [
+      {
+        model: Contract,
+        where: {
+          ...profileToQuery(profile),
+        },
+        attributes: [],
+      },
+    ],
+    where: {
+      paid: false,
+    },
+  });
+
+  res.json({ data: jobs });
 };
 
 const pay = async (req, res) => {
